669a95a8ff004e06a01c82057213ef6b1061475b
HngChau <23130029@st.hcmuaf.edu.vn>
2025-11-05 16:24:40 +0700

    Add files via upload

--- Assets/js/products.js
+++ Assets/js/products.js
@@ -9,3 +9,36 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+// Lấy các phần tử cần thao tác
+const filterBtn = document.getElementById("filter-btn");
+const filterPanel = document.getElementById("filter-panel");
+
+// Khi bấm nút "Bộ lọc"
+filterBtn.addEventListener("click", () => {
+    // Nếu panel đang mở → đóng lại
+    if (filterPanel.classList.contains("active")) {
+        filterPanel.classList.remove("active");
+        document.body.classList.remove("filter-open");
+    }
+    // Nếu panel đang đóng -> mở ra
+    else {
+        filterPanel.classList.add("active");
+        document.body.classList.add("filter-open");
+    }
+});
+
+// Khi bấm ra ngoài phần panel -> đóng lại
+document.addEventListener("click", (e) => {
+    if (
+        !filterPanel.contains(e.target) &&
+        !filterBtn.contains(e.target)
+    ) {
+        filterPanel.classList.remove("active");
+        document.body.classList.remove("filter-open");
+    }
+});
+
+
+
+
+
